912a4b7cd123b6236c24383acec7c3c3a48d1782
IjazKakkodDS <ijazkakkod@gmail.com>
2025-01-10 01:07:59 +0000

    Hyperparameter Tuning Completed

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+from src.logger import logging
 
 from src.exception import CustomException
 
@@ -22,26 +23,27 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
+        for i, (model_name, model) in enumerate(models.items()):
+            try:
+                para = param[model_name]
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
 
-            model.fit(X_train,y_train)
-           
-            y_train_pred = model.predict(X_train)
+                y_train_pred = model.predict(X_train)
+                y_test_pred = model.predict(X_test)
 
-            y_test_pred = model.predict(X_test)
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
 
-            train_model_score = r2_score(y_train,y_train_pred)
-
-            test_model_score = r2_score(y_test,y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
-
+                report[model_name] = test_model_score
+            except Exception as e:
+                logging.error(f"Model {model_name} failed: {e}")
         return report
-
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
